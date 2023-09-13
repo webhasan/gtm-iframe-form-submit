01f5492a883a72d956355f7f999e67c7e90ec5e4
Md Hasanuzzaman <webhasan24@gmail.com>
2023-09-13 15:04:03 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,106 @@
 (function() {
-  /**
-   * Author: Md Hasanuzzaman
-   * Eamil: webhasan24@gmail.com
-   * Linkedin: https://linkedin.com/md-h
-   */
-
-    var iframeSelector = 'iframe'; // change this  as your iframe selector
-
-    var iframe = document.querySelector(iframeSelector);
-    var iframeHeightAfterSubmission = .5;
-    var isFormSubmitted = false;
-    var isInsideIframe = false;
-    var isCodeExecuted = false;
-    var iframeHeight;
-
-    var observer = new MutationObserver(function(mutationsList, observer) {
-
-       var currentHeight = iframe.offsetHeight;
-
-       if(currentHeight <  iframeHeight * iframeHeightAfterSubmission && !isFormSubmitted) {
-        observer.disconnect();
-
-        isFormSubmitted = true;
-        window.dataLayer = window.dataLayer || [];
-        dataLayer.push({
-            event: 'iframe_form_submit',
-            form_location: window.location.href,
-            iframe_id: iframe.getAttribute('id'),
-            iframe_class: iframe.getAttribute('class')
+    function leoMeasureIframeFormSubmitDataLayer() {
+        /**
+         * Author: Md Hasanuzzaman
+         * Email: webhasan24@gmail.com
+         * Linkedin: https://linkedin.com/md-h
+         * Youtube: https://www.youtube.com/@LeoMeasure-nr3pf
+         */
+    
+        var iframeSelector = 'iframe'; // change this as your iframe selector
+    
+        var iframe = document.querySelector(iframeSelector);
+        var isFormSubmitted = false;
+        var isInsideIframe = false;
+        var isCodeExecuted = false;
+        var iframeHeight;
+    
+        var observer = new MutationObserver(function (_mutationsList, observer) {
+            var currentHeight = iframe.offsetHeight;
+            var iframeHeightChange = Math.abs(((currentHeight - iframeHeight) / iframeHeight) * 100);
+    
+            if (!isFormSubmitted && iframeHeightChange > 40) {
+                observer.disconnect();
+    
+                isFormSubmitted = true;
+                window.dataLayer = window.dataLayer || [];
+                dataLayer.push({
+                    event: 'iframe_form_submit',
+                    form_location: window.location.href,
+                    iframe_id: iframe.getAttribute('id'),
+                    iframe_class: iframe.getAttribute('class')
+                });
+            }
         });
-       }
-    });
-
-    function handleMouseOver(event) {
-        if (event.target.closest(iframeSelector)) {
-            isInsideIframe = true;
-        } else {
-            isInsideIframe = false;
+    
+        function handleMouseOver(event) {
+            if (event.target.closest(iframeSelector)) {
+                isInsideIframe = true;
+            } else {
+                isInsideIframe = false;
+            }
         }
-    }
-
-    document.addEventListener('mouseover', handleMouseOver);
-    window.addEventListener('blur', function() {
-
-        if(isInsideIframe && !isCodeExecuted) {
-             document.removeEventListener('mouseover', handleMouseOver);
-
-            window.dataLayer = window.dataLayer || [];
-            dataLayer.push({
-                event: 'iframe_form_start',
-                form_location: window.location.href,
-                iframe_id: iframe.getAttribute('id'),
-                iframe_class: iframe.getAttribute('class')
+    
+        function handleFormSubmission() {
+            var formInsideIframe = iframe.contentDocument.querySelector('form');
+    
+            formInsideIframe.addEventListener('submit', function (event) {
+                var formData = {};
+                var formInputs = formInsideIframe.querySelectorAll('input, select, textarea');
+    
+                for (var i = 0; i < formInputs.length; i++) {
+                    var input = formInputs[i];
+                    if (input.type === 'radio') {
+    
+                        if (input.checked) {
+                            formData[input.name] = input.value;
+                        }
+                    } else if (input.type === 'checkbox') {
+                        if (!formData[input.name]) {
+                            formData[input.name] = [];
+                        }
+                        if (input.checked) {
+                            formData[input.name].push(input.value);
+                        }
+                    } else {
+                        formData[input.name] = input.value;
+                    }
+                }
+    
+                window.dataLayer = window.dataLayer || [];
+                window.dataLayer.push({
+                    event: 'iframe_form_submit',
+                    form_location: window.location.href,
+                    iframe_id: iframe.getAttribute('id'),
+                    iframe_class: iframe.getAttribute('class'),
+                    user_inputs: formData
+                });
             });
-
-             isCodeExecuted = true;
-             iframeHeight  = iframe.offsetHeight;
-             observer.observe(iframe, { attributes: true, childList: true, subtree: true });
         }
-    });
-})()
+    
+        document.addEventListener('mouseover', handleMouseOver);
+        window.addEventListener('blur', function () {
+    
+            if (isInsideIframe && !isCodeExecuted) {
+                isCodeExecuted = true;
+                document.removeEventListener('mouseover', handleMouseOver);
+    
+                window.dataLayer = window.dataLayer || [];
+                dataLayer.push({
+                    event: 'iframe_form_start',
+                    form_location: window.location.href,
+                    iframe_id: iframe.getAttribute('id'),
+                    iframe_class: iframe.getAttribute('class')
+                });
+    
+                if (iframe.contentDocument) {
+                    handleFormSubmission();
+                } else {
+                    iframeHeight = iframe.offsetHeight;
+                    observer.observe(iframe, { attributes: true, childList: true, subtree: true });
+                }
+            }
+        });
+    }
+    leoMeasureIframeFormSubmitDataLayer();
+})
